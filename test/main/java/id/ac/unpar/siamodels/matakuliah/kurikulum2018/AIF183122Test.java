c1ae57afa26fc02e180575ecb7044c2709ab2f27
Stevin <michael.destiny.slayer@gmail.com>
2019-04-04 14:45:51 +0700

    Completition 100% Coverage

---
+++ test/main/java/id/ac/unpar/siamodels/matakuliah/kurikulum2018/AIF183122Test.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.unpar.siamodels.matakuliah.kurikulum2018;
+
+import id.ac.unpar.siamodels.Mahasiswa;
+import id.ac.unpar.siamodels.MataKuliah;
+import id.ac.unpar.siamodels.MataKuliahFactory;
+import id.ac.unpar.siamodels.TahunSemester;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AIF183122Test {
+    
+    /**
+     * Tes cek method cek prasyarat yang berhasil dari kelas AIF183122.
+     */
+    @Test
+    public void testCheckPrasyaratYangBerhasil() {
+        AIF182101 asd = new AIF182101();
+        AIF183122 pemodelanDanSimulasi = new AIF183122();
+        
+        Mahasiswa mahasiswa = new Mahasiswa("2018730079");
+        MataKuliahFactory mkf = MataKuliahFactory.getInstance();
+        MataKuliah mk = mkf.createMataKuliah("AIF182101", 3, "asd");
+        TahunSemester ts = new TahunSemester("181");
+
+        Mahasiswa.Nilai nilai = new Mahasiswa.Nilai(ts, mk, "A");
+        
+        mahasiswa.getRiwayatNilai().add(nilai);
+        
+        List<String> reasonsContainer = new LinkedList<String>();
+        boolean expRes = true;
+        boolean res = pemodelanDanSimulasi.checkPrasyarat(mahasiswa, reasonsContainer);
+        assertEquals(expRes, res);
+    }
+    
+    
+    /**
+     * Tes cek method cek prasyarat yang gagal dari kelas AIF183122.
+     */
+    @Test
+    public void testCheckPrasyaratYangGagal() {
+        AIF183122 pemodelanDanSimulasi = new AIF183122();
+        
+        Mahasiswa mahasiswa = new Mahasiswa("2018730079");
+        MataKuliahFactory mkf = MataKuliahFactory.getInstance();
+        TahunSemester ts = new TahunSemester("181");
+        
+        List<String> reasonsContainer = new LinkedList<String>();
+        boolean expRes = false;
+        boolean res = pemodelanDanSimulasi.checkPrasyarat(mahasiswa, reasonsContainer);
+        assertEquals(expRes, res);
+    }
+    
+}
